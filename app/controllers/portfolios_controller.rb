f94b2d8fa742ff8460908b839246e565ea4b5de5
jithin R <jeets2689@gmail.com>
2017-10-24 19:07:07 +0530

    integrated new and create action and created a show file

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,22 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+def new
+ @portfolio = Portfolio.new
+end
+def create
+ @portfolio = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
+ if @portfolio.save
+ 	redirect_to portfolios_path
+ else
+ 	render 'new'
+ end
+end
 
+def show
+ #@portfolio = Portfolio.find(params(:id))
+end
 	
+
+ 
 end
