f0d905673e06e7efc66d6f1731b38d8db842f7db
jithin R <jeets2689@gmail.com>
2017-10-30 21:13:27 +0530

    configured routes and modified landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+  root 'pages#home'
 
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
   get 'pages/contact'
 
