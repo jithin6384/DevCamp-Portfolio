0b6720e443b16dc2886bc802cd605a23209f290a
jithin R <jeets2689@gmail.com>
2017-12-15 22:05:52 +0530

    completed authentication with virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_presence_of :name
+     def first_name
+         self.name.split.first
+     end
+
+     def middle_name
+     	q = self.name.split(" ")
+        if q.length >= 2
+        	middle_name = q[1]
+        	return middle_name
+        else
+        	last_name
+        end
+     end
+
+     def last_name
+     	self.name.split.last
+      
+      end
+
 end
